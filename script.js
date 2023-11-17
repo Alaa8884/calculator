765758146443d29c6c971faa3a2aa2958a8a0d91
Alaa8884 <alaamuhamed8@gmail.com>
2023-11-17 19:20:03 +0300

    initialize project

---
+++ script.js
@@ -0,0 +1,23 @@
+// let input = document.querySelector('input[type="text"]')
+// let button = document.querySelector('[type="button"]');
+
+// let string = '';
+// let arr = Array.from(button);
+
+// arr.forEach(ele => {
+//   ele.addEventListener('click', (e) => {
+//     if (e.target.innerHTML = '=') {
+//       string = eval(string)
+//       input.value = string
+//     } else if (e.target.innerHTML = 'AC') {
+//       string = ''
+//       input.value = string
+//     } else if (e.target.innerHTML = 'DE') {
+//       string = string.slice(0, -1)
+//       input.value = string
+//     } else {
+//       string += e.target.innerHTML
+//       input.value = string
+//     }
+//   } )
+// })
